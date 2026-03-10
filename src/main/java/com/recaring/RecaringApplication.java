dbee2d8052cbe05a004914cf32f3eb263e716d40
brian <brian506@naver.com>
2026-03-10 14:52:42 +0900

    init project

---
+++ src/main/java/com/recaring/RecaringApplication.java
@@ -0,0 +1,13 @@
+package com.recaring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecaringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecaringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recaring/RecaringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recaring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecaringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
