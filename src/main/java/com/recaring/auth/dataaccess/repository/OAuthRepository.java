4457808e3e926f8653cc72493dc1b8a4d7cd9fb0
brian <brian506@naver.com>
2026-07-03 17:03:21 +0900

    feature[#134] : 로컬 회원가입 필수 + 사후 소셜 로그인 연동

--- src/main/java/com/recaring/auth/dataaccess/repository/OAuthRepository.java
+++ src/main/java/com/recaring/auth/dataaccess/repository/OAuthRepository.java
@@ -9,7 +9,5 @@ import java.util.Optional;
 
 public interface OAuthRepository extends JpaRepository<OAuth, Long>, OAuthRepositoryCustom {
 
-    Optional<OAuth> findByProviderAndProviderMemberId(OAuthProvider provider, String providerMemberId);
-
     boolean existsByMemberKeyAndProvider(String memberKey, OAuthProvider provider);
 }
